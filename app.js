e008cfc81dc788dbe3c00ae38c5b123288f07a1d
Pablo Campuzano Cuadrado <pablo.camcua@educa.jcyl.es>
2024-10-25 14:09:26 +0200

    a

--- app.js
+++ app.js
@@ -12,6 +12,12 @@ let victoria = false;
 let tablas = false;
 let turnoActual = 0;
 
+let tablero = 
+    [
+        [null, null, null],
+        [null, null, null], //guardamos el estado de la partida en un array
+        [null, null, null],
+    ]
 
 function comprobarCasillaValida(casilla) {
     let contenido = casilla.textContent;
@@ -20,6 +26,7 @@ function comprobarCasillaValida(casilla) {
 
 function ejecutarTurno(casilla) {
     casilla.textContent = FICHAS[turnoActual % 2];
+    tablero[casilla/3][casilla%3] = FICHAS[turnoActual % 2]; //en el array si es 7 coge la casilla 7/3 = 2; 7%3 = 1 -> 2,1
     turnoActual++;
 }
 
@@ -29,10 +36,56 @@ function comprobarTablas() {
     }
 }
 
-function comprobarHorizontal() {}
-function comprobarVertical() {}
-function comprobarDiagonalPrincipal() {}
-function comprobarDiagonalSecundaria() {}
+function comprobarHorizontal(numeroCasilla) {
+    let numeroX = numeroCasilla/3
+    let numeroY = numeroCasilla%3
+    if(numeroCasilla <= 3){
+        if(tablero[numeroX][numeroY] === tablero[numeroX+1][numeroY] && tablero[numeroX][numeroY] === tablero[numeroX+2][numeroY]){
+            return true;
+        }
+    }
+    else if(numeroCasilla <= 6){
+        if(tablero[numeroX][numeroY] === tablero[numeroX+1][numeroY] && tablero[numeroX][numeroY] === tablero[numeroX-1][numeroY]){
+            return true;
+        }
+    }
+    else {
+        if(tablero[numeroX][numeroY] === tablero[numeroX-1][numeroY] && tablero[numeroX][numeroY] === tablero[numeroX-2][numeroY]){
+            return true;
+        }
+    }
+    return false;
+}
+function comprobarVertical() {
+    if(numeroCasilla%3 == 1){
+        if(tablero[numeroX][numeroY] === tablero[numeroX][numeroY+1] && tablero[numeroX][numeroY] === tablero[numeroX][numeroY+2]){
+            return true;
+        }
+    }
+    else if(numeroCasilla <= 6){
+        if(tablero[numeroX][numeroY] === tablero[numeroX][numeroY+1] && tablero[numeroX][numeroY] === tablero[numeroX][numeroY-1]){
+            return true;
+        }
+    }
+    else {
+        if(tablero[numeroX][numeroY] === tablero[numeroX][numeroY-1] && tablero[numeroX][numeroY] === tablero[numeroX][numeroY-2]){
+            return true;
+        }
+    }
+    return false;
+}
+function comprobarDiagonalPrincipal() {
+    if(tablero[0][0] == tablero[1][1] && tablero[0][0] == tablero[2][2]){
+        return true;
+    }
+    return false
+}
+function comprobarDiagonalSecundaria() {
+    if(tablero[0][0] == tablero[1][1] && tablero[0][0] == tablero[2][2]){
+        return true;
+    }
+    return false;
+}
 
 function comprobarFinDeJuego(casilla) {
     const numeroCasilla = casilla.textContent;
@@ -45,6 +98,8 @@ function comprobarFinDeJuego(casilla) {
     if(DIAGONAL_SECUNDARIA.includes(numeroCasilla)) {
         comprobarDiagonalSecundaria(numeroCasilla);
     }
+
+    
 
     comprobarTablas();
 
